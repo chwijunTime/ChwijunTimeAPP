39b952abb29fb84f982543e3d9ecd1c4f94f424b
ansuvin <dkstnqls0925@naver.com>
2021-04-06 12:02:30 +0900

    [ADD] TextFormField

--- flutter_app/app_user/lib/widgets/text_field.dart
+++ flutter_app/app_user/lib/widgets/text_field.dart
@@ -4,17 +4,30 @@ Widget buildTextField(String hint, TextEditingController controller) {
   return TextField(
     controller: controller,
     decoration: InputDecoration(
-      hintText: hint,
-      hintStyle: TextStyle(
-        color: Colors.grey,
-        fontWeight: FontWeight.w500
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.blueAccent)
-      ),
-      enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.grey)
-      )
-    ),
+        hintText: hint,
+        hintStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.w500),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blueAccent)),
+        enabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
   );
+}
+
+Widget buildTextFormField(
+    String hint, TextEditingController controller, String msg) {
+  return TextFormField(
+      controller: controller,
+      validator: (value) {
+        if (value.trim().isEmpty) {
+          return msg;
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+          hintText: hint,
+          hintStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.w500),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.blueAccent)),
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))));
 }
